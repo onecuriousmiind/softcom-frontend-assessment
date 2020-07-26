0854a0a25f9aed85ac25c71b7838c80daf5bde87
beelaw <lawrence.mcbhee@gmail.com>
2020-07-26 02:11:50 +0100

    Fixes typo and grammar errors in Button component's entry file code comment.

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,11 @@
 /**
- * This is will serve as the base for all buttons used
+ * This will serve as the base for all buttons used
  * app-wide. This can easily be customized or extended by
  * throwing in a custom Button component that wraps the
  * atlaskit Button.
  *
- * Also at this point, only the needed Button component
- * from atlaskit is exported leaving every other pieces out;
+ * Also, at this point, only the needed Button component
+ * from atlaskit is exported leaving other pieces out;
  * this keeps the bundle small, thanks to Webpack's
  * tree shaking algorithm.
  *
