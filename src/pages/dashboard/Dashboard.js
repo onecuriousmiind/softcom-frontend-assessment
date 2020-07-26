a154530822b731d3c41e77374d6d2188a97dc4ce
beelaw <lawrence.mcbhee@gmail.com>
2020-07-26 01:11:30 +0100

    Sets up app pages/routes

---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Dashboard extends PureComponent {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div>
+        Dashboard page works!
+      </div>
+    );
+  }
+}
---
+++ src/pages/get-started/GetStarted.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class GetStarted extends PureComponent {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div>
+        Get started page works!
+      </div>
+    );
+  }
+}
