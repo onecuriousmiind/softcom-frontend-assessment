14915652888569ff2cad1a333a061aa9c44aafa8
beelaw <lawrence.mcbhee@gmail.com>
2020-07-26 02:41:32 +0100

    Creates form components

---
+++ src/components/form/index.js
@@ -0,0 +1,13 @@
+/**
+ * This will serve as the base for all things "form" used
+ * app-wide. These can easily be customized or extended by
+ * wrapping the form components with custom ones.
+ *
+ * Also, at this point, only the needed form components
+ * from atlaskit are exported, leaving other pieces out;
+ * this keeps the bundle small, thanks to Webpack's
+ * tree shaking algorithm.
+ *
+ */
+
+export { default, Field, FormSection, FormFooter } from '@atlaskit/form';
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './button';
 export { default as TextField } from './textfield';
+export { default as Form, Field as FormField, FormSection, FormFooter } from './form';
