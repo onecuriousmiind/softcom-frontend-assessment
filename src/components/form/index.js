f1d635c4118f61f75816504a53b09bac20070adb
beelaw <lawrence.mcbhee@gmail.com>
2020-07-29 08:48:53 +0100

    Exports custom FormHeader and FormSection in form components barrel file

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,17 +1,12 @@
 /**
  * This will serve as the base for all things "form" used
- * app-wide. These can easily be customized or extended by
- * wrapping the form components with custom ones.
- *
- * Also, at this point, only the needed form components
- * from atlaskit are exported, leaving other pieces out;
- * this keeps the bundle small, thanks to Webpack's
- * tree shaking algorithm.
+ * app-wide.
  *
  */
 
 export { default } from './Form';
-export { FormHeader, FormSection, FormFooter } from '@atlaskit/form';
+export { default as FormHeader } from './FormHeader';
+export { default as FormSection } from './FormSection';
 export { default as FormField } from './FormField';
 export { default as EmailFormField } from './EmailFormField';
 export { default as FullNameFormField } from './FullNameFormField';
